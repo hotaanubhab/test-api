178b0e3ca4274a8459deb13a4fa4cc23bd78fcb2
Anubhab Hota <hota000anubhab@gmail.com>
2021-10-17 19:46:54 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+const http = require('http');
+const express = require('express');
+const socketio = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
+
+io.on('connection', (socket) => {
+    socket.on('joinChannelChat', (clubId) => {
+        console.log(`${socket.id} joined channel with id ${clubId}`);
+        socket.join(clubId);
+    });
+
+    socket.on('chatMessage', ({clubId, chatInfo}) => {
+        io.to(clubId).emit('message', chatInfo);
+    });
+});
+
+const PORT = 8080;
+server.listen(PORT, () => {
+    console.log('Server started');
+});
