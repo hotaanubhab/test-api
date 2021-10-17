ed43ebb2cc28dbe5ecb2238f373a1b3419b744ad
Anubhab Hota <hota000anubhab@gmail.com>
2021-10-17 19:48:31 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,7 +17,7 @@ io.on('connection', (socket) => {
     });
 });
 
-const PORT = process.env.port || 8080;
+const PORT = process.env.PORT || 8080;
 server.listen(PORT, () => {
     console.log('Server started');
 });
